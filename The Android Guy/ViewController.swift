74028a45eabe0f7f563372186942de44d9c899ac
student cce <student.cce@studentccedeMacBook-Air.local>
2017-10-07 18:18:42 +0800

    Initial Commit

---
+++ The Android Guy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Android Guy
+//
+//  Created by student.cce on 2017/10/7.
+//  Copyright © 2017年 student.cce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
